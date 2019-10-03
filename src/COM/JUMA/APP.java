f861e7767a3039c67327e06821c3e7eba34bff41
AC09 <stephen.wanyingo@gmail.com>
2019-10-03 15:48:11 +0300

    first  commit

---
+++ src/COM/JUMA/APP.java
@@ -0,0 +1,27 @@
+package COM.JUMA;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class APP {
+    private JPanel PNAME;
+    private JButton BUTTON_msg;
+
+    public APP() {
+        BUTTON_msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"hello world");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("APP");
+        frame.setContentPane(new APP().PNAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
